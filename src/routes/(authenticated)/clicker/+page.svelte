c593731b1eca468a30df3ce31a356c60aa917cd6
Schmul2004 <samuel04mc@gmail.com>
2023-06-07 09:27:43 +0200

    fix

--- src/routes/(authenticated)/clicker/+page.svelte
+++ src/routes/(authenticated)/clicker/+page.svelte
@@ -9,16 +9,16 @@
 	$: clicks = data.clicks;
 </script>
 
-<div class="flex justify-center">
+<!-- <div class="flex justify-center">
 	<h2 class="text-2xl">Clicks: {clicks}</h2>
-</div>
+</div> -->
 
 <div class="flex items-center justify-end">
 	<div class="flex items-center card w-96 shadow-xl space-y-4 mx-8">
 		<div class="flex flex-col py-8 space-y-8">
 			<div class="form-control flex-col space-y-8">
 				<h1 class="flex items-center justify-center card-title text-3xl font-bold">Leaderboard</h1>
-				<h3 class="text-xl">Peoples:{data.users}</h3>
+				<h3 class="text-xl">Clicks:{data.clicks}</h3>
 			</div>
 		</div>
 	</div>
